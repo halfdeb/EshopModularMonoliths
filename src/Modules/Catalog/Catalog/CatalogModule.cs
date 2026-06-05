4afc82671d56fcae1a516aa83dab856b573fd970
halfdeb <deb13136@gmail.com>
2026-06-06 00:30:42 +0530

    Wire-up Dependencies between Modules

---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Catalog;
+
+public static class CatalogModule
+{
+    public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+
+    public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
+    {
+        return app;
+    }
+}
