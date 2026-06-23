932775fbae8040cbd62e0e0f7e61c29ca13bb710
halfdeb <deb13136@gmail.com>
2026-06-24 00:09:33 +0530

    V2 Develop Catalog API Endpoints

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,4 +1,17 @@
+using Shared.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddCarter(configurator: config =>
+// {
+//     var catalogModules = typeof(CatalogModule).Assembly.GetTypes()
+//         .Where(t => t.IsAssignableTo(typeof(ICarterModule))).ToArray();
+//
+//     config.WithModules(catalogModules);
+// });
+
+builder.Services
+    .AddCarterWithAssemblies(typeof(CatalogModule).Assembly);
 
 //Add services to the container.
 builder.Services
@@ -7,6 +20,8 @@ builder.Services
     .AddOrderingModule(builder.Configuration);
 
 var app = builder.Build();
+
+app.MapCarter();
 
 //Configure the HTTP request pipeline.
 app
